805273f1d40718439cc31a30417133c79548679d
Mateoo-pierre <minomateo02@gmail.com>
2021-08-08 02:36:42 -0300

    Back-End

--- PI-Dogs/client/src/App.js
+++ PI-Dogs/client/src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter, Route} from 'react-router-dom'
+
+//import css
 import './App.css';
+
+//import component
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <h1 className = 'title_home'>Henry Dogs</h1>
+      <div className='div_buton_home'>
+        <a className = 'buton_home' href="">INGRESAR</a>
+      </div>
     </div>
   );
 }
